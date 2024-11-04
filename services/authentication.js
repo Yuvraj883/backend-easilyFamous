41a4126267ab4dba50f68882748126fa8c58ff45
Yuvraj <devyuvraj883@gmail.com>
2024-11-05 02:55:34 +0530

    Created generate token function

---
+++ services/authentication.js
@@ -0,0 +1,19 @@
+const { Fullscreen } = require('@mui/icons-material');
+const JWT = require('jsonwebtoken');
+require('dotenv').config();
+
+const secret = process.env.SECRET_KEY;
+
+function generateToken(user){
+  const payload = {
+    _id: user._id,
+    fullName: user.fullName,
+    email: user.email,
+    profileImage: user.profileImage
+  }
+  const token = JWT.sign(payload, secret, {expiresIn:'2h'});
+  return token;
+}
+
+
+module.exports = {generateToken};
