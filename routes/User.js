de3239911e465c6bc9b3012b0bb40ced1a9371bc
Yuvraj <devyuvraj883@gmail.com>
2024-11-05 03:24:27 +0530

    Renamed the User.js to user.js

--- routes/User.js
+++ routes/User.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const { generateToken } = require('../services/authentication');
 
 router.post('/sign-in', async (req, res)=>{
   const {email, password} = req.body;
@@ -14,6 +15,22 @@ router.post('/sign-in', async (req, res)=>{
     res.status(401).send({message:"Incorrect email or password"});
   }
 
-})
+});
+
+router.post('/sign-up', async(req,res)=>{
+  const {fullName, email, password} = req.body;
+  const newUser = await User.create({
+    fullName,
+    email,
+    password,
+  });
+  const token = generateToken(newUser);
+  res.cookie('token', token, {
+    httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+  }).redirect('/');
+});
+
 
 module.exports = router;
