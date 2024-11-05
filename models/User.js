ac39a206df78badfcdb3037baa06c900a1ead6f4
Yuvraj <devyuvraj883@gmail.com>
2024-11-05 19:07:28 +0530

    Fixed User model

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const { Schema, model } = require('mongoose');
 const { createHmac, randomBytes } = require('crypto');
-const {generateToken} = require('../services/authentication');
+const { generateToken } = require('../services/authentication');
+
 const userSchema = Schema(
   {
     fullName: {
@@ -26,35 +27,34 @@ const userSchema = Schema(
     },
   },
   { timestamps: true }
-)
+);
 
+// Pre-save hook to hash the password
 userSchema.pre('save', function (next) {
-  const user = this
+  if (!this.isModified('password')) return next();
 
-  if (!user.isModified(user.password)) return
-  const salt = randomBytes(16).toString('hex')
-  const hashedPassword = createHmac('sha256', salt)
-    .update(user.password)
-    .digest('hex')
-  this.salt = salt
-  this.password = user.password
-  next()
-})
+  const salt = randomBytes(16).toString('hex');
+  const hashedPassword = createHmac('sha256', salt).update(this.password).digest('hex');
+  this.salt = salt;
+  this.password = hashedPassword;
+  next();
+});
 
-userSchema.static('matchPasswordAndGenerateToken', async function(email, password){
-  const user = await this.findOne(email);
-  if(!user){
+// Static method to match password and generate token
+userSchema.static('matchPasswordAndGenerateToken', async function (email, password) {
+  const user = await this.findOne({ email });
+  if (!user) {
     throw new Error("User doesn't exist");
   }
-  const salt = user.salt;
-  const hashedPassword = createHmac('sha256',salt ).update(password).digest('hex');
-  if(hashedPassword!==password){
+
+  const hashedPassword = createHmac('sha256', user.salt).update(password).digest('hex');
+  if (hashedPassword !== user.password) {
     throw new Error("Wrong password");
   }
- const token = generateToken(user);
- return token;
 
-})
+  const token = generateToken(user);
+  return token;
+});
 
-const User = model('User', userSchema)
-module.exports = User
+const User = model('User', userSchema);
+module.exports = User;
