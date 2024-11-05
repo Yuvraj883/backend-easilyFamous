960f21e972ce8c3059d714ce420a158c277729b2
Yuvraj <devyuvraj883@gmail.com>
2024-11-05 18:46:00 +0530

    Added home route

--- app.js
+++ app.js
@@ -2,9 +2,22 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 require('dotenv').config();
+const userRoute = require('./routes/User');
+const User = require('./models/User');
 
 mongoose.connect(process.env.MONGO_URI).then(()=>{console.log("DB connected")}).catch((e)=>{console.log("Mongoose error: ",e)});
 
 const PORT = 8000;
+app.use(express.json());
+
+app.use('/user',userRoute );
+app.get('/', async(req, res)=>{
+  // const user = await User.create({
+  //   fullName:"Yuvraj Singh",
+  //   email:"devyuvraj883@gmail.com",
+  //   password:"Yuvraj123"
+  // });
+  res.send("Welcome");
+})
 
 app.listen(PORT, ()=>{console.log(`Server started at port ${PORT}`)})
